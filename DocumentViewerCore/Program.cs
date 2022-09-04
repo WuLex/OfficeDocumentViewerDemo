272b13264fc6b052195ac644e06ba3e8b389d089
wulex <827937686@qq.com>
2022-09-04 11:46:04 +0800

    升级net6

--- DocumentViewerCore/Program.cs
+++ DocumentViewerCore/Program.cs
@@ -1,26 +1,45 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using DocumentViewerCore.Common;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
-namespace DocumentViewerCore
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient("default", c =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    c.BaseAddress = new Uri("https://localhost");
+    c.DefaultRequestHeaders.Add("Accept", "application/json");
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+}
+
+HttpContextHelper.Configure(app.Services.GetRequiredService<IHttpContextAccessor>());
+HttpContextHelper.Configure(app.Services.GetRequiredService<IWebHostEnvironment>());
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+app.Run();
